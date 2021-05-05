7e524f586f3a1762e2d7b39add7e8449c3e58528
Aqsa Kanwal <wasifkhan994@gmail.com>
2021-05-05 23:51:21 +0500

    Created XML for Login and Register Page.

--- app/src/main/java/com/example/foodriderapp/MainActivity.java
+++ app/src/main/java/com/example/foodriderapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.foodriderapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Stack Overflow Code:
+        View decorView = getWindow().getDecorView();
+// Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+// Remember that you should never show the action bar if the
+// status bar is hidden, so hide that too if necessary.
+        ActionBar actionBar = getActionBar();
+        actionBar.hide();
+        //Stack Overflow Code End.
     }
 }
