10baee0a62ee99fa02e3337558edb80f1812abbf
Aqsa Kanwal <wasifkhan994@gmail.com>
2021-05-07 01:23:16 +0500

    Fixed bugs related to Login and Register Fuctionalities.

--- app/src/main/java/com/example/foodriderapp/MainActivity.java
+++ app/src/main/java/com/example/foodriderapp/MainActivity.java
@@ -15,17 +15,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //Stack Overflow Code:
-        View decorView = getWindow().getDecorView();
-// Hide the status bar.
-        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
-        decorView.setSystemUiVisibility(uiOptions);
-// Remember that you should never show the action bar if the
-// status bar is hidden, so hide that too if necessary.
-        ActionBar actionBar = getActionBar();
-        actionBar.hide();
-        //Stack Overflow Code End.
     }
 
     public void logout(View view)
