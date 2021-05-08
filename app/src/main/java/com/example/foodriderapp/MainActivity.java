321fa9f26329eb5d82e9bfccdaebff7a31e570e0
Aqsa Kanwal <wasifkhan994@gmail.com>
2021-05-08 22:18:43 +0500

    Added google Map API.

--- app/src/main/java/com/example/foodriderapp/MainActivity.java
+++ app/src/main/java/com/example/foodriderapp/MainActivity.java
@@ -6,15 +6,27 @@ import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button startButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.startShift);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),MapsActivity.class));
+            }
+        });
     }
 
     public void logout(View view)
@@ -23,4 +35,5 @@ public class MainActivity extends AppCompatActivity {
         startActivity(new Intent(getApplicationContext(),Login.class));
         finish();
     }
+
 }
